af0671acd8508793be557042a13c84d6a75fae80
anusha-developer <83368436+anusha-developer@users.noreply.github.com>
2021-08-27 21:58:02 +0530

    Add files via upload

---
+++ Employee.aspx
@@ -0,0 +1,95 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Employee.aspx.cs" Inherits="ASPNETCURD.Employee" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+                <asp:HiddenField ID="hfEmpID" runat="server" />
+            <table>
+                <tr>
+                    <td>
+                        <asp:Label ID="Label1" runat="server" Text="EmpName"></asp:Label>
+                    </td>
+                     <td colspan="2">
+                         <asp:TextBox ID="txtEmpName" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                 <tr>
+                    <td>
+                        <asp:Label ID="Label2" runat="server" Text="EmpDesignation"></asp:Label>
+                    </td>
+                     <td colspan="2">
+                         <asp:TextBox ID="txtEmpDesignation" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                 <tr>
+                    <td>
+                        <asp:Label ID="Label3" runat="server" Text="EmpMobileno"></asp:Label>
+                    </td>
+                     <td colspan="2">
+                         <asp:TextBox ID="txtEmpmobileno" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                 <tr>
+                    <td>
+                        <asp:Label ID="Label4" runat="server" Text="EmpGender"></asp:Label>
+                    </td>
+                     <td colspan="2">
+                         <asp:TextBox ID="txtEmpGender" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Label ID="Label5" runat="server" Text="EmpAddress"></asp:Label>
+                    </td>
+                     <td colspan="2">
+                         <asp:TextBox ID="txtEmpAddress" runat="server" TextMode="MultiLine" ></asp:TextBox>
+                    </td>
+                </tr>
+                 <tr>
+                     <td colspan="2">
+                         <asp:Button ID="btnSave" runat="server" Text="Save" OnClick="btnSave_Click" />
+                         <asp:Button ID="btnDelete" runat="server" Text="Delete" OnClick="btnDelete_Click" />
+                         <asp:Button ID="btnClear" runat="server" Text="Clear" OnClick="btnClear_Click" />
+
+                    </td>
+                </tr>
+                    <tr>
+                     <td colspan="2">
+                         <asp:Label ID="lblSuccessMessage" runat="server" Text="" ForeColor="Green"></asp:Label>
+
+                    &nbsp;
+
+                    </td>
+                </tr>
+                   <tr>
+                     <td colspan="2">
+                         <asp:Label ID="lblErrorMessage" runat="server" Text="" ForeColor="red"></asp:Label>
+
+                    </td>
+                </tr>
+            </table>
+            <br />
+            <asp:GridView ID="gvEmployee" runat="server" AutoGenerateColumns="false">
+                <Columns>
+                    <asp:BoundField DataField="EmpName" HeaderText="EmpName" />
+                    <asp:BoundField DataField="EmpDesignation" HeaderText="EmpDesignation" />
+                    <asp:BoundField DataField="EmpMobileno" HeaderText="EmpMobileno" />
+                    <asp:BoundField DataField="EmpGender" HeaderText="EmpGender" />
+                    <asp:BoundField DataField="EmpAddress" HeaderText="EmpAddress" />
+                    <asp:TemplateField>
+                        <ItemTemplate>
+                            <asp:LinkButton ID="lnkview" runat="server" CommandArgument='<%# Eval("EmpID")%>' OnClick="lnk_onClick">View</asp:LinkButton>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                </Columns>
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Employee.aspx.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASPNETCURD
+{
+    public partial class Employee : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(@"data source=.; database=ASPNETCURD; integrated security=SSPI");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack)
+            {
+                btnDelete.Enabled = false;
+                FillGridView();
+            }
+
+        }
+
+        protected void btnClear_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+        public void Clear()
+        {
+            //hf means hiddenfeild
+            hfEmpID.Value = "";
+
+            txtEmpName.Text = txtEmpDesignation.Text = txtEmpmobileno.Text = txtEmpGender.Text = txtEmpAddress.Text = "";
+            btnSave.Text = "Save";
+            lblSuccessMessage.Text = lblErrorMessage.Text = "";
+            btnDelete.Enabled = false;
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            if (con.State == System.Data.ConnectionState.Closed)
+                con.Open();
+            SqlCommand cmd = new SqlCommand("EmployeeCreateUpdate", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@EmpID", hfEmpID.Value == "" ? 0 : Convert.ToInt32(hfEmpID.Value));
+            cmd.Parameters.AddWithValue("@EmpName", txtEmpName.Text.Trim());
+            cmd.Parameters.AddWithValue("@EmpDesignation", txtEmpDesignation.Text.Trim());
+            cmd.Parameters.AddWithValue("@EmpMobileno", txtEmpmobileno.Text.Trim());
+            cmd.Parameters.AddWithValue("@EmpGender", txtEmpGender.Text.Trim());
+            cmd.Parameters.AddWithValue("@EmpAddress", txtEmpAddress.Text.Trim());
+            cmd.ExecuteNonQuery();
+            con.Close();
+            string EmpID = hfEmpID.Value;
+            Clear();
+            if (EmpID =="")
+                lblSuccessMessage.Text = "Saved Successfully";
+            else
+                lblSuccessMessage.Text = "Updated successfully";
+            FillGridView();
+        }
+        void FillGridView()
+        {
+            if (con.State == System.Data.ConnectionState.Closed)
+                con.Open();
+            SqlDataAdapter sdr = new SqlDataAdapter("ASPCURD_EmployeeViewall", con);
+            sdr.SelectCommand.CommandType = CommandType.StoredProcedure;
+            DataTable dbtbl = new DataTable();
+
+            sdr.Fill(dbtbl);
+            con.Close();
+            gvEmployee.DataSource = dbtbl;
+            gvEmployee.DataBind();
+       
+        }
+        public void lnk_onClick(object sender, EventArgs e)
+        {
+            int EmpID = Convert.ToInt32((sender as LinkButton).CommandArgument);
+            if (con.State == System.Data.ConnectionState.Closed)
+             con.Open();
+            SqlDataAdapter sdr = new SqlDataAdapter("ASPCURD_EmployeeViewById", con);
+            sdr.SelectCommand.CommandType = CommandType.StoredProcedure;
+            sdr.SelectCommand.Parameters.AddWithValue("@EmpID", EmpID);
+            DataTable dbtbl = new DataTable();
+            sdr.Fill(dbtbl);
+            con.Close();
+            hfEmpID.Value = EmpID.ToString();
+            txtEmpName.Text = dbtbl.Rows[0]["EmpName"].ToString();
+            txtEmpDesignation.Text = dbtbl.Rows[0]["EmpDesignation"].ToString();
+            txtEmpGender.Text = dbtbl.Rows[0]["EmpGender"].ToString();
+            txtEmpmobileno.Text = dbtbl.Rows[0]["Empmobileno"].ToString();
+            txtEmpAddress.Text = dbtbl.Rows[0]["EmpAddress"].ToString();
+            btnSave.Text = "Updated";
+            btnDelete.Enabled = true;
+
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (con.State == System.Data.ConnectionState.Closed)
+                con.Open();
+            SqlCommand cmd = new SqlCommand("ASPCURD_EmployeedDeleteById",con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@EmpID", (Convert.ToInt32(hfEmpID.Value)));
+            cmd.ExecuteNonQuery();
+            con.Close();
+            Clear();
+            FillGridView();
+            lblSuccessMessage.Text = "Deleted Successfully";
+        }
+    }
+}
